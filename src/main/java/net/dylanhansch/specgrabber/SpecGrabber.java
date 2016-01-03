804902e3a1338565da961cd049bc587e77c0d019
Dylan Hansch <dylan@dylanhansch.net>
2016-01-03 02:11:52 -0600

    Plugin Init + Maven

---
+++ src/main/java/net/dylanhansch/specgrabber/SpecGrabber.java
@@ -0,0 +1,15 @@
+package net.dylanhansch.specgrabber;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SpecGrabber extends JavaPlugin {
+	
+	@Override
+	public void onDisable(){
+	}
+	
+	@Override
+	public void onEnable(){
+	}
+	
+}
